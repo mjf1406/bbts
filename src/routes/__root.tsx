ef4f1be8c20198a9dbc4ef0088dd6bcf5dff4e16
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2026-01-14 08:22:54 +0900

    google

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,23 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import { ThemeProvider } from '@/components/themes/theme-provider'
 import Navbar from '@/components/navigation/app-navbar'
 import AuthProvider from '@/components/auth/auth-provider'
 
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID!
+
 export const Route = createRootRoute({
   component: () => (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AuthProvider>
-        <Navbar />
-        <Outlet />
-      </AuthProvider>
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        {' '}
+        <AuthProvider>
+          <Navbar />
+          <Outlet />
+        </AuthProvider>
+      </GoogleOAuthProvider>
       <TanStackDevtools
         config={{
           position: 'bottom-right',
