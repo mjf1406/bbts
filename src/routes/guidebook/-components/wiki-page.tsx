e48f99c05fc0e87cb1fc59a59e79a774ae1f5948
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2026-01-14 13:46:42 +0900

    guidebook has nice suspense now

--- src/routes/guidebook/-components/wiki-page.tsx
+++ src/routes/guidebook/-components/wiki-page.tsx
@@ -1,19 +1,95 @@
 import * as React from 'react'
-import { WikiTableOfContents } from './wiki-table-of-contents'
-import { WikiDisclaimer } from './wiki-disclaimer'
-import { WikiWelcome } from './wiki-welcome'
-import { WikiTransformationPrinciples } from './wiki-transformation-principles'
-import { WikiProgramOverview } from './wiki-program-overview'
-import { WikiExerciseSubstitutions } from './wiki-exercise-substitutions'
-import { WikiWhatsNew } from './wiki-whats-new'
-import { WikiTrainingGear } from './wiki-training-gear'
-import { WikiProgressTracking } from './wiki-progress-tracking'
-import { WikiMuscleAnatomy } from './wiki-muscle-anatomy'
-import { WikiNutrition } from './wiki-nutrition'
-import { WikiSupplements } from './wiki-supplements'
-import { WikiFAQ } from './wiki-faq'
-import { WikiComments } from './wiki-comments'
-import { WikiReferences } from './wiki-references'
+import { Suspense } from 'react'
+import { Skeleton } from '@/components/ui/skeleton'
+
+// Lazy load all wiki components
+const WikiTableOfContents = React.lazy(() =>
+  import('./wiki-table-of-contents').then((module) => ({
+    default: module.WikiTableOfContents,
+  })),
+)
+const WikiDisclaimer = React.lazy(() =>
+  import('./wiki-disclaimer').then((module) => ({
+    default: module.WikiDisclaimer,
+  })),
+)
+const WikiWelcome = React.lazy(() =>
+  import('./wiki-welcome').then((module) => ({
+    default: module.WikiWelcome,
+  })),
+)
+const WikiTransformationPrinciples = React.lazy(() =>
+  import('./wiki-transformation-principles').then((module) => ({
+    default: module.WikiTransformationPrinciples,
+  })),
+)
+const WikiProgramOverview = React.lazy(() =>
+  import('./wiki-program-overview').then((module) => ({
+    default: module.WikiProgramOverview,
+  })),
+)
+const WikiExerciseSubstitutions = React.lazy(() =>
+  import('./wiki-exercise-substitutions').then((module) => ({
+    default: module.WikiExerciseSubstitutions,
+  })),
+)
+const WikiWhatsNew = React.lazy(() =>
+  import('./wiki-whats-new').then((module) => ({
+    default: module.WikiWhatsNew,
+  })),
+)
+const WikiTrainingGear = React.lazy(() =>
+  import('./wiki-training-gear').then((module) => ({
+    default: module.WikiTrainingGear,
+  })),
+)
+const WikiProgressTracking = React.lazy(() =>
+  import('./wiki-progress-tracking').then((module) => ({
+    default: module.WikiProgressTracking,
+  })),
+)
+const WikiMuscleAnatomy = React.lazy(() =>
+  import('./wiki-muscle-anatomy').then((module) => ({
+    default: module.WikiMuscleAnatomy,
+  })),
+)
+const WikiNutrition = React.lazy(() =>
+  import('./wiki-nutrition').then((module) => ({
+    default: module.WikiNutrition,
+  })),
+)
+const WikiSupplements = React.lazy(() =>
+  import('./wiki-supplements').then((module) => ({
+    default: module.WikiSupplements,
+  })),
+)
+const WikiFAQ = React.lazy(() =>
+  import('./wiki-faq').then((module) => ({
+    default: module.WikiFAQ,
+  })),
+)
+const WikiComments = React.lazy(() =>
+  import('./wiki-comments').then((module) => ({
+    default: module.WikiComments,
+  })),
+)
+const WikiReferences = React.lazy(() =>
+  import('./wiki-references').then((module) => ({
+    default: module.WikiReferences,
+  })),
+)
+
+// Loading fallback component
+function WikiSectionSkeleton() {
+  return (
+    <div className="space-y-4 py-8">
+      <Skeleton className="h-8 w-3/4" />
+      <Skeleton className="h-4 w-full" />
+      <Skeleton className="h-4 w-full" />
+      <Skeleton className="h-4 w-5/6" />
+    </div>
+  )
+}
 
 const tocItems = [
   { id: 'disclaimer', title: 'Disclaimer', level: 0 },
@@ -54,25 +130,63 @@ export function WikiPage() {
             The Bodybuilding Transformation System
           </h1>
 
-          <WikiDisclaimer />
-          <WikiWelcome />
-          <WikiTransformationPrinciples />
-          <WikiProgramOverview />
-          <WikiExerciseSubstitutions />
-          <WikiWhatsNew />
-          <WikiTrainingGear />
-          <WikiProgressTracking />
-          <WikiMuscleAnatomy />
-          <WikiNutrition />
-          <WikiSupplements />
-          <WikiFAQ />
-          <WikiComments />
-          <WikiReferences />
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiDisclaimer />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiWelcome />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiTransformationPrinciples />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiProgramOverview />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiExerciseSubstitutions />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiWhatsNew />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiTrainingGear />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiProgressTracking />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiMuscleAnatomy />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiNutrition />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiSupplements />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiFAQ />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiComments />
+          </Suspense>
+          <Suspense fallback={<WikiSectionSkeleton />}>
+            <WikiReferences />
+          </Suspense>
         </main>
 
         {/* Table of Contents Sidebar */}
         <aside className="hidden md:block">
-          <WikiTableOfContents items={tocItems} />
+          <Suspense
+            fallback={
+              <div className="space-y-2">
+                <Skeleton className="h-6 w-32" />
+                <Skeleton className="h-4 w-40" />
+                <Skeleton className="h-4 w-36" />
+              </div>
+            }
+          >
+            <WikiTableOfContents items={tocItems} />
+          </Suspense>
         </aside>
       </div>
     </div>
